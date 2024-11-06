20a33ace622474ce739f8b64361bc8be4288aa8f
Archana Singh <archana.23b0131132@abes.ac.in>
2024-11-06 15:44:56 +0530

    added files

---
+++ leapyear.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int yr;
+    cout<<"enter the year:";
+    cin>>yr;
+    if((yr%4==0) && (yr%100!=0))
+    cout<<"\n it is a leap year";
+else if(yr%400==0)
+cout<<"\n it is a leap year";
+else
+cout<<"\n it is a leap year";
+cout<<endl;
+return 0;
+}
