a1e56e5758f0c24077b5f79ac5f49ac6f42e7c68
Archana Singh <archana.23b0131132@abes.ac.in>
2024-11-17 19:27:58 +0530

    Add files via upload
    uploaded

---
+++ area.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+    int r,area,a;
+    cout<<"enter the radius:";
+    cin>>r;
+    a=2*r*r;
+    cout<<"the area of largest possible ractangle (square)"<<a<<endl;
+
+    return 0;
+
+}
